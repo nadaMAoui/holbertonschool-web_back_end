65eed98fbcecd294625796326c11e1227f74567b
nadaMAoui <maaouinada747@gmail.com>
2024-02-15 20:31:27 +0100

    asynch_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ from random import uniform
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator:
     """
     Loop 10 times, each time asynchronously wait 1 second,
     then yield a random number between 0 and 10.
@@ -13,12 +13,3 @@ async def async_generator() -> AsyncGenerator[float, None, None]:
     for _ in range(10):
         await asyncio.sleep(1)
         yield uniform(0, 10)
-
-# Example usage:
-async def main():
-    async for number in async_generator():
-        print(number)
-
-# Run the event loop manually
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
