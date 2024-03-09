42be525a70107ac3acb1a14dfdb9907a7991401b
nadaMAoui <maaouinada747@gmail.com>
2024-03-09 22:04:26 +0100

    nada

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -1,59 +1,47 @@
 #!/usr/bin/env python3
-'''Testing
-'''
+"""test client"""
 import unittest
-from unittest import mock
 from client import GithubOrgClient
 from parameterized import parameterized
-from unittest.mock import PropertyMock, Mock, patch
+from unittest.mock import PropertyMock, patch
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    '''
-    a test class that inherits from unittest.TestCase
-    '''
+    """"""
     @parameterized.expand([
-        ("google"),
-        ("abc"),
+        ["google"],
+        ["abc"],
     ])
     @patch("client.get_json")
-    def test_org(self, url, payload, get_patch):
-        '''
-        test that GithubOrgClient.org returns the correct value.
-        '''
-        get_patch.return_value = payload
-        github_client = GithubOrgClient(url)
-        response = github_client.org
-        self.assertEqual(response, payload)
-        get_patch.assert_called_once()
+    def test_org(self, url, payload):
+        """ Method that tests GithubOrgClient """
+        test_class = GithubOrgClient(url)
+        self.assertEqual(test_class.org, payload.return_value)
+        payload.assert_called_once()
 
     def test_public_repos_url(self):
-        '''
-        Test that the result of _public_repos_url
-        is the expected one based on the mocked payload.
-        '''
+        """Test that the result of _public_repos_url
+        is the expected one based on the mocked payload."""
         with patch.object(GithubOrgClient,
                           "org",
-                          new_callable=PropertyMock) as mock_o:
-            test_json = {"url": "facebook",
-                         "repos_url": "http://taylorswift.com"}
-            mock_o.return_value = test_json
+                          new_callable=PropertyMock) as patched:
+            test_json = {"url": "linkedin",
+                         "repos_url": "http://google.com"}
+            patched.return_value = test_json
             github_client = GithubOrgClient(test_json.get("url"))
             response = github_client._public_repos_url
-            mock_o.assert_called_once()
+            patched.assert_called_once()
             self.assertEqual(response, test_json.get("repos_url"))
 
     @patch("client.get_json")
     def test_public_repos(self, get_patch):
-        '''
-        Test that the list of repos is what you expect from the chosen payload.
-        '''
+        """"""
         get_patch.return_value = [{"name": "google"},
                                   {"name": "abc"}]
         with patch.object(GithubOrgClient, "_public_repos_url",
                           new_callable=PropertyMock) as mock_o:
-            mock_o.return_value = "http://taylorswift.com"
-            github_client = GithubOrgClient("facebook")
+            mock_o.return_value = "http://linkedin.com"
+            github_client = GithubOrgClient("yahoo")
             response = github_client.public_repos()
             self.assertEqual(response, ["google", "abc"])
             get_patch.assert_called_once()
@@ -64,15 +52,11 @@ class TestGithubOrgClient(unittest.TestCase):
         ({"license": {"key": "other_license"}}, "my_license", False)
     ])
     def test_has_license(self, repo, license, expected):
-        '''
-        Testing for test_has_license
-        '''
-        github_client = GithubOrgClient("facebook")
+        """"""
+        github_client = GithubOrgClient("yahoo")
         response = (github_client.has_license(repo, license))
         self.assertEqual(response, expected)
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
