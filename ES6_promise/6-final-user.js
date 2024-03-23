f35b1a3a40a8f23a1928d2da5d5d0d7a26b0c917
nadaMAoui <maaouinada747@gmail.com>
2024-03-23 19:41:08 +0100

    final user

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,20 +1,18 @@
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
+
 export default async function handleProfileSignup(
   firstName,
   lastName,
   fileName
 ) {
-  const arr = [];
-  arr.push(
-    {
-      status: "fulfilled",
-      value: await signUpUser(firstName, lastName).then((response) => response),
-    },
-    {
-      status: "rejected",
-      value: await uploadPhoto(fileName).catch((err) => err),
-    }
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((res) =>
+    res.map((o) => ({
+      status: o.status,
+      value: o.status === "fulfilled" ? o.value : String(o.reason),
+    }))
   );
-  return arr;
 }
