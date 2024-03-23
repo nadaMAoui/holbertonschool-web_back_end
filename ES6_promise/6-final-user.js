573601980b3980a7eb307463e0117f436633e296
nadaMAoui <maaouinada747@gmail.com>
2024-03-23 19:29:22 +0100

    final user

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,20 @@
-// Handle multiple promises
 import signUpUser from "./4-user-promise";
 import uploadPhoto from "./5-photo-reject";
-
-function handleProfileSignup(firstName = "", lastName = "", fileName = "") {
-  return Promise.allSettled([
-    uploadPhoto(fileName),
-    signUpUser(firstName, lastName),
-  ]);
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
+  const arr = [];
+  arr.push(
+    {
+      status: "fulfilled",
+      value: await signUpUser(firstName, lastName).then((response) => response),
+    },
+    {
+      status: "rejected",
+      value: await uploadPhoto(fileName).catch((err) => err),
+    }
+  );
+  return arr;
 }
-
-export default handleProfileSignup;
