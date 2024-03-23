5cfd1859880d91b99fde0d0db0e290d1a4ec38af
nadaMAoui <maaouinada747@gmail.com>
2024-03-23 19:53:23 +0100

    corr

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,10 +1,6 @@
-// Divide by 0
 export default function divideFunction(numerator, denominator) {
   if (denominator === 0) {
     throw Error("cannot divide by 0");
   }
-
-  const result = numerator / denominator;
-
-  return result;
+  return numerator / denominator;
 }
