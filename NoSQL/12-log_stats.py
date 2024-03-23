6cc02ea3e2e9cb122a19e1eb047fcad467dabb2f
nadaMAoui <maaouinada747@gmail.com>
2024-03-23 14:35:47 +0100

    log stat>>

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,28 +1,28 @@
+#!/usr/bin/env python3
+"""
+Write a Python script that provides some stats
+about Nginx logs stored in MongoDB.
+"""
 import pymongo
 
-# Connect to MongoDB
-client = pymongo.MongoClient("mongodb://localhost:27017/")  # Replace with your MongoDB connection string
-db = client["logs"]
-collection = db["nginx"]
 
-# Get total number of documents
-total_logs = collection.count_documents({})
+if __name__ == "__main__":
+    """
+    Prints out the amount of logs,
+    the amount of logs with each REST method
+    and all the status checks.
+    """
+    CLIENT = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    DB = CLIENT.logs
+    COLLECTION = DB.nginx
 
-# Get method counts
-method_counts = collection.aggregate([
-    {"$group": {"_id": "$method", "count": {"$sum": 1}}},
-    {"$sort": {"_id": 1}}
-])
+    LOG_COUNT = COLLECTION.count_documents({})
+    print(f"{LOG_COUNT} logs")
 
-# Get count for method=GET, path=/status
-status_count = collection.count_documents({"method": "GET", "path": "/status"})
+    print("Methods:")
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
+        METHOD_COUNT = COLLECTION.count_documents({"method": method})
+        print(f"\tmethod {method}: {METHOD_COUNT}")
 
-# Print results
-print(f"{total_logs} logs")
-print("Methods:")
-for method_count in method_counts:
-    print(f"\t{method_count['count']} {method_count['_id']}")
-print(f"\t{status_count} GET /status")
-
-# Close MongoDB connection
-client.close()
+    STATUS_LOG_COUNT = COLLECTION.count_documents({"path": "/status"})
+    print(f"{STATUS_LOG_COUNT} status check")
