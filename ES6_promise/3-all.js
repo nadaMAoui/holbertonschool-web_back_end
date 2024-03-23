220ea38685862d97beaf72f3ec11d8c0b4350223
nadaMAoui <maaouinada747@gmail.com>
2024-03-23 19:18:38 +0100

    all

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,14 +1,12 @@
 import { uploadPhoto, createUser } from "./utils.js";
-export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((messages) => {
-      console.log(
-        messages[0].body,
-        messages[1].firstName,
-        messages[1].lastName
-      );
-    })
-    .catch(() => {
-      console.log("Signup system offline");
-    });
+
+async function handleProfileSignup() {
+  try {
+    const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+    console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
+  } catch (error) {
+    console.error("Signup system offline");
+  }
 }
+
+export default handleProfileSignup;
