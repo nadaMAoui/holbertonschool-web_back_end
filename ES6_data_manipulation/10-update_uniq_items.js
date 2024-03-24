75cb87dccdd6ac344e43f5e62a65a409b757ccda
nadaMAoui <maaouinada747@gmail.com>
2024-03-24 13:32:13 +0100

    corr

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,14 +1,9 @@
-export default function updateUniqueItems(groceryList) {
-  // returns a map with updated map items.
-  //changes 'rice' => 1 to 'rice' => 100.
-  //if not a map return Cannot process
-
-  if (groceryList instanceof Map) {
-    for (let product of groceryList) {
-      if (product[1] === 1) groceryList.set(product[0], 100);
+export default function updateUniqueItems(map) {
+    try {
+      map.forEach((value, key) => {
+        if (value === 1) { map.set(key, 100); }
+      });
+    } catch (e) {
+      throw Error('Cannot process');
     }
-    return groceryList;
-  } else {
-    console.log("Cannot Process");
   }
-}
