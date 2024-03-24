41e2151b8fb4e8437112136227ab372803f37328
nadaMAoui <maaouinada747@gmail.com>
2024-03-24 11:44:37 +0100

    hbtn course

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,39 +1,48 @@
 export default class HolbertonCourse {
     constructor(name, length, students) {
-      this.name = name;
-      this.length = length;
-      this.students = students;
+      if (typeof name !== 'string') {
+        throw Error('Name must be a string');
+      }
+      if (typeof length !== 'number') {
+        throw Error('Length must be a number');
+      }
+      if (students.every((i) => (typeof i !== 'string'))) {
+        throw Error('Students must be an Array of strings');
+      }
+      this._name = name;
+      this._length = length;
+      this._students = students;
     }
   
     get name() {
       return this._name;
     }
   
-    set name(name) {
-      if ((typeof name !== 'string') && !(name instanceof String)) {
-        throw new TypeError('Name must be a string');
-      }
-      this._name = name;
-    }
-  
     get length() {
       return this._length;
-    }
-  
-    set length(length) {
-      if ((typeof length !== 'number') && !(length instanceof Number)) {
-        throw new TypeError('Length must be a number');
-      }
-      this._length = length;
     }
   
     get students() {
       return this._students;
     }
   
+    set name(name) {
+      if (typeof name !== 'string') {
+        throw Error('Name must be a string');
+      }
+      this._name = name;
+    }
+  
+    set length(length) {
+      if (typeof length !== 'number') {
+        throw Error('Length must be a number');
+      }
+      this._length = length;
+    }
+  
     set students(students) {
-      if (!(students instanceof Array) || !students.every((s) => typeof s === 'string')) {
-        throw new TypeError('Students must be an array of strings');
+      if (students.every((i) => typeof i !== 'string')) {
+        throw Error('Students must be an Array of strings');
       }
       this._students = students;
     }
