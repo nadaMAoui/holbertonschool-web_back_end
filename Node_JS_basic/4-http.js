6ddfed2acf521663fa2e577da2e75e3ff81bbc98
nadaMAoui <maaouinada747@gmail.com>
2024-03-24 19:24:36 +0100

    http

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,17 +1,16 @@
-const http = require("http");
+const http = require('http');
 
+const hostname = '127.0.0.1';
 const port = 1245;
 
 const app = http.createServer((req, res) => {
-  res.write("Hello Holberton School!");
-  res.end();
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!');
 });
 
-app.listen(port, (error) => {
-  if (error) {
-    console.log("Something went wrong", error);
-  } else {
-    console.log(`Server is listening on port ${port}`);
-  }
+app.listen(port, hostname, () => {
+  //   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
 module.exports = app;
