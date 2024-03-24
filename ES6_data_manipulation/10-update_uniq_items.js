fd4f57e1b73262861ebbeb0a551cca449668c183
nadaMAoui <maaouinada747@gmail.com>
2024-03-24 13:46:52 +0100

    update

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,13 +1,13 @@
 const updateUniqueItems = (groceryMap) => {
-	if (!(groceryMap instanceof Map)) {
-	  throw new Error('Cannot process');
-	}
-  
-	groceryMap.forEach((quantity, item) => {
-	  if (quantity === 1) {
-		groceryMap.set(item, 100);
-	  }
-	});
-  };
-  
-  export default updateUniqueItems;
+  if (!(groceryMap instanceof Map)) {
+    throw new Error("Cannot process");
+  }
+
+  groceryMap.forEach((quantity, item) => {
+    if (quantity === 1) {
+      groceryMap.set(item, 100);
+    }
+  });
+};
+
+export default updateUniqueItems;
