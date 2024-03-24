394b1173c06fd929bc944f5e88abcabd2fa21db6
nadaMAoui <maaouinada747@gmail.com>
2024-03-24 11:48:26 +0100

    sky high

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -1,20 +1,24 @@
-import Building from "./5-building";
+// Extend Buildinf
+import Building from './5-building';
 
-export default class SkyHighBuilding extends Building {
+/**
+ * Define Class
+ *
+ * @class SkyHighBuilding
+ */
+class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
   }
 
-  get sqft() {
-    return this._sqft;
-  }
-
   get floors() {
-    return this._floors;
+    return (this._floors);
   }
 
   evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors`;
+    return (`Evacuate slowly the ${this.floors} floors`);
   }
 }
+
+export default SkyHighBuilding;
